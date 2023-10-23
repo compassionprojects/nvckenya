ae2976600f495e9f2bd9a9174103149f35f043c1
madhums <me@madhums.me>
2023-10-23 21:58:07 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
         <a href="/">Home</a> &nbsp; <a href="/donate">Donate</a>
       </div>
       <footer className="mt-5 pb-5 text-center text-body-tertiary small">
-        Made with &#x2764; by Madhu using Gatsby.js &#183;{' '}
+        Made with &#x2764; by Madhu &#183;{' '}
         <a
           className="text-reset text-decoration-none pb-1"
           style={{ borderBottom: '1px dashed' }}
